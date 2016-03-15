faa368c1d0e3fa4efd8c7401c59bd7faddd6842d
bhanuc <bhanuc@iitk.ac.in>
2016-03-16 02:28:00 +0530

    test build, removed history for cleaner look

---
+++ README.md
@@ -0,0 +1,86 @@
+# alphonso
+
+Your own private offline npm registry and backup server.
+
+Overview
+--------
+
+This project allows you to have your own npm registry. This server works with the necessary `npm` commands just like the npmjs.org registry. You can use it to not worry about npm going down or to store your private packages. It performs much faster than npmjs.org and can even be matched with a CDN like Cloudfront to be fast globally.
+
+Rather than trying to copy all the data in npm, this acts more like a proxy. While npm is up, it will cache package data onto the local disk. If npm goes down, it will deliver whatever is available in the cache. This means it won't be a fully comprehensive backup of npm, but you will be able to access anything you accessed before.
+
+This project is an independent fork of [elephant](https://github.com/dickeyxxx/elephant) which was inspired from [sinopia](https://github.com/rlidwka/sinopia). This came out of the need of running a local npm cache that was independent of the internet. This fork will remain independent due to conflicts of goals of the project, for more details see this [issue](https://github.com/dickeyxxx/elephant/issues/9#issuecomment-196822043)
+
+This is also a [12 Factor](http://12factor.net/) app to make it easy to host on a PaaS like Heroku or in a custom Ansible/Chef/Puppet cluster.
+
+Setup
+-----
+
+If `REDIS_URL` is set (optional) redis will be used to cache package data.
+
+The easiest way to set this up is with the Heroku button:
+
+[![Deploy to Heroku](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+Alternatively, you can set it up by cloning this repo:
+
+```
+$ git clone https://github.com/navya/alphonso
+$ cd alphonso
+$ npm install
+$ npm start
+```
+
+Either way, your registry is now setup and you should be able to test it by updating the packages with it:
+
+```
+$ npm update --registry http://urltomyregistry
+```
+
+See below for how to enable authorization and `npm publish`.
+
+How it works
+------------
+
+Essentially the goal of the project is to quickly deliver current npm data even when npm is offline.  In npm there are 2 main types of requests: package metadata and tarballs.
+
+Package metadata mostly contains what versions of a package are available. These cannot be cached for very long since the package can be updated. By default, it is cached for 60 seconds. You can modify this with `CACHE_PACKAGE_TTL`. Etags are also supported and cached to further speed up access.
+
+The tarballs are the actual code and never change once they are uploaded (though they can be removed via unpublishing). These are downloaded one time from npmjs.org per package and version, stored in S3 and in the local tmp folder for future requests. These have a very long max-age header.
+
+In the event npmjs.org is offline, elephant will use the most recent package metadata that was requested from npmjs.org until it comes back online.
+
+npm commands supported
+----------------------
+
+* `npm install`
+* `npm update`
+* `npm login`
+* `npm whoami`
+* `npm publish`
+
+Authentication
+--------------
+
+Alphonso uses an htpasswd file in local disk for authentication and stores tokens in local disk. To set this up, first create an htpasswd file. Then upload it to `/htpasswd` in the main project directory.
+
+```
+$ htpasswd -nB YOURUSERNAME >> ./htpasswd
+```
+
+Then you can login with npm. Note that the email is ignored by the server, but the CLI will force you to add one.
+
+```
+$ npm login --registry http://myregistry
+Username: dickeyxxx
+Password:
+Email: (this IS public) jeff@heroku.com
+$ npm whoami --registry http://myregistry
+dickeyxxx
+```
+
+This stores the credentials in `~/.npmrc`. You can now use `npm publish` to publish packages.
+
+**NOTE**: Because the original use-case for having private packages was a little strange, right now you need to be authenticated to upload a private package, but once they are in the registry anyone can install them (but they would have to know the name of it). Comment open a new issue on the project if you'd like to see better functionality around this.
+
+**EXTRA:NOTE**: Test-suite is not yet added but will be added soon.
---
+++ server.js
@@ -0,0 +1,177 @@
+'use strict';
+
+if (!process.env.NEW_RELIC_LICENSE_KEY) process.env.NEW_RELIC_ENABLED = 'false';
+process.env.NEW_RELIC_LOG = 'stdout';
+
+let koa      = require('koa');
+let compress = require('koa-compress');
+let r        = require('koa-route');
+let newrelic = require('newrelic');
+let parse    = require('co-body');
+let fs       = require('fs');
+let packages = require('./lib/packages');
+let tarballs = require('./lib/tarballs');
+let config   = require('./lib/config');
+let user     = require('./lib/user');
+let rollbar  = require('rollbar');
+let path     = require('path');
+let app      = koa();
+
+if (config.rollbar) {
+  rollbar.init(config.rollbar, {
+    environment:  process.env.NODE_ENV,
+    codeVersion:  process.env.HEROKU_SLUG_COMMIT,
+    host:         process.env.HEROKU_APP_NAME,
+    scrubHeaders: ['authorization'],
+  });
+  rollbar.handleUncaughtExceptions(config.rollbar);
+}
+
+app.name = 'Alphanso';
+app.port = config.port;
+
+app.use(require('./logger'));
+app.use(compress());
+
+// static root page
+app.use(r.get('/', function* () {
+  newrelic.setTransactionName('');
+  this.type = 'text/html';
+  this.body = fs.createReadStream(__dirname + '/public/index.html');
+}));
+
+// ping
+app.use(r.get('/-/ping', function *() {
+  newrelic.setTransactionName('-/ping');
+  this.body = {};
+}));
+
+
+// error middleware
+app.use(function* (next) {
+  try { yield next; }
+  catch (err) {
+    newrelic.noticeError(err);
+    if (config.rollbar) rollbar.handleError(err, this.request);
+    this.status = err.status || 500;
+    this.body   = {error: err.message};
+    this.app.emit('error', err, this);
+  }
+});
+
+// get package metadata
+app.use(r.get('/:name', function *(name) {
+  newrelic.setTransactionName(':name');
+  let etag = this.req.headers['if-none-match'];
+  let pkg = yield packages(this.metric).get(name, etag);
+  if (pkg === 304) {
+    this.status = 304;
+    return;
+  }
+  if (pkg === 404) {
+    this.status = 404;
+    this.body   = {error: 'no such package available'};
+    return;
+  }
+  let cloudfront = this.headers['user-agent'] === 'Amazon CloudFront';
+  packages(this.metric).rewriteTarballURLs(pkg, cloudfront ? config.cloudfrontHost : this.headers.host);
+  this.set('ETag', pkg.etag);
+  this.set('Cache-Control', `public, max-age=${config.cache.packageTTL}`);
+  this.body = pkg;
+}));
+
+// get package tarball with sha
+app.use(r.get('/:name/-/:filename/:sha', function *(name, filename, sha) {
+  newrelic.setTransactionName(':name/-/:filename/:sha');
+  let tarball = yield tarballs(this.metric, this).get(name, filename, sha);
+  if (!tarball) {
+    this.status = 404;
+    this.body   = {error: 'no tarball found'};
+    return;
+  }
+  this.set('Content-Length', tarball.size);
+  this.set('Cache-Control', `public, max-age=${config.cache.tarballTTL}`);
+  this.body = tarball;
+}));
+
+// get package tarball with sha
+app.use(r.get('/:scope/:name/-/:filename/:sha', function *(scope, name, filename, sha) {
+  newrelic.setTransactionName(':scope/:name/-/:filename/:sha');
+  let tarball = yield tarballs(this.metric,this).get(`${scope}/${name}`, filename, sha);
+  if (!tarball) {
+    this.status = 404;
+    this.body   = {error: 'no tarball found'};
+    return;
+  }
+  this.set('Content-Length', tarball.size);
+  this.set('Cache-Control', `public, max-age=${config.cache.tarballTTL}`);
+  this.body = tarball;
+}));
+
+// get package tarball without sha
+app.use(r.get('/:name/-/:filename', function *(name, filename) {
+  newrelic.setTransactionName(':name/-/:filename');
+  let ext = path.extname(filename);
+  filename = path.basename(filename, ext);
+  this.redirect(`/${name}/-/${filename}/a${ext}`);
+}));
+
+// login
+app.use(r.put('/-/user/:user', function *() {
+  newrelic.setTransactionName('-/user/:user');
+  let auth = yield user.authenticate(yield parse(this));
+  if (auth) {
+    this.status = 201;
+    this.body   = {token: auth};
+  } else {
+    this.status = 401;
+    this.body   = {error: "invalid credentials"};
+  }
+}));
+
+// authenticate
+app.use(function* (next) {
+  if (!this.headers.authorization) {
+    this.status = 401;
+    this.body   = {error: 'no credentials provided'};
+    return;
+  }
+  let token = this.headers.authorization.split(' ')[1];
+  this.username = yield user.findByToken(token);
+  if (!this.username) {
+    this.status = 401;
+    this.body   = {error: 'invalid credentials'};
+    return;
+  }
+  yield next;
+});
+
+// whoami
+app.use(r.get('/-/whoami', function *() {
+  newrelic.setTransactionName('-/whoami');
+  this.body = {username: this.username};
+}));
+
+// npm publish
+app.use(r.put('/:name', function *() {
+  newrelic.setTransactionName(':name');
+  let pkg = yield parse(this);
+  try {
+    yield packages(this.metric).upload(pkg);
+    this.body = yield packages(this.metric).get(pkg.name);
+  } catch (err) {
+    if (err === packages(this.metric).errors.versionExists) {
+      this.body   = {error: err.toString()};
+      this.status = 409;
+    } else {
+      throw err;
+    }
+  }
+}));
+
+module.exports = app;
+if (!module.parent) {
+  app.listen(app.port, function () {
+    console.error(`${app.name} listening on port ${app.port} [${app.env}]`);
+  });
+}
