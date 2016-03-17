0524556db4bdf60bfc713a49b29998d3d23bd945
bhanuc <bhanuc@iitk.ac.in>
2016-03-17 09:20:32 +0530

    bug-fix and added a new route to handle package

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 'use strict';
 
 
-let koa      = require('koa');
+let koa = require('koa');
 let compress = require('koa-compress');
-let r        = require('koa-route');
-let parse    = require('co-body');
-let fs       = require('fs');
+let r = require('koa-route');
+let parse = require('co-body');
+let fs = require('fs');
 let packages = require('./lib/packages');
 let tarballs = require('./lib/tarballs');
-let config   = require('./lib/config');
-let user     = require('./lib/user');
-let rollbar  = require('rollbar');
-let path     = require('path');
-let app      = koa();
+let config = require('./lib/config');
+let user = require('./lib/user');
+let rollbar = require('rollbar');
+let path = require('path');
+let app = koa();
 
 if (config.rollbar) {
   rollbar.init(config.rollbar, {
-    environment:  process.env.NODE_ENV,
-    codeVersion:  process.env.HEROKU_SLUG_COMMIT,
-    host:         process.env.HEROKU_APP_NAME,
+    environment: process.env.NODE_ENV,
+    codeVersion: process.env.HEROKU_SLUG_COMMIT,
+    host: process.env.HEROKU_APP_NAME,
     scrubHeaders: ['authorization'],
   });
   rollbar.handleUncaughtExceptions(config.rollbar);
@@ -31,30 +31,33 @@ app.use(require('./logger'));
 app.use(compress());
 
 // static root page
-app.use(r.get('/', function* () {
+app.use(r.get('/', function * () {
   this.type = 'text/html';
   this.body = fs.createReadStream(__dirname + '/public/index.html');
 }));
 
 // ping
-app.use(r.get('/-/ping', function *() {
+app.use(r.get('/-/ping', function * () {
   this.body = {};
 }));
 
 
 // error middleware
-app.use(function* (next) {
-  try { yield next; }
-  catch (err) {
+app.use(function * (next) {
+  try {
+    yield next;
+  } catch (err) {
     if (config.rollbar) rollbar.handleError(err, this.request);
     this.status = err.status || 500;
-    this.body   = {error: err.message};
+    this.body = {
+      error: err.message
+    };
     this.app.emit('error', err, this);
   }
 });
 
 // get package metadata
-app.use(r.get('/:name', function *(name) {
+app.use(r.get('/:name', function * (name) {
   let etag = this.req.headers['if-none-match'];
   let pkg = yield packages(this.metric).get(name, etag);
   if (pkg === 304) {
@@ -63,7 +66,9 @@ app.use(r.get('/:name', function *(name) {
   }
   if (pkg === 404) {
     this.status = 404;
-    this.body   = {error: 'no such package available'};
+    this.body = {
+      error: 'no such package available'
+    };
     return;
   }
   let cloudfront = this.headers['user-agent'] === 'Amazon CloudFront';
@@ -74,11 +79,13 @@ app.use(r.get('/:name', function *(name) {
 }));
 
 // get package tarball with sha
-app.use(r.get('/:name/-/:filename/:sha', function *(name, filename, sha) {
+app.use(r.get('/:name/-/:filename/:sha', function * (name, filename, sha) {
   let tarball = yield tarballs(this.metric, this).get(name, filename, sha);
   if (!tarball) {
     this.status = 404;
-    this.body   = {error: 'no tarball found'};
+    this.body = {
+      error: 'no tarball found'
+    };
     return;
   }
   this.set('Content-Length', tarball.size);
@@ -87,12 +94,14 @@ app.use(r.get('/:name/-/:filename/:sha', function *(name, filename, sha) {
 }));
 
 // get package tarball with sha
-app.use(r.get('/:scope/:name/-/:filename/:sha', function *(scope, name, filename, sha) {
+app.use(r.get('/:scope/:name/-/:filename/:sha', function * (scope, name, filename, sha) {
   newrelic.setTransactionName(':scope/:name/-/:filename/:sha');
-  let tarball = yield tarballs(this.metric,this).get(`${scope}/${name}`, filename, sha);
+  let tarball = yield tarballs(this.metric, this).get(`${scope}/${name}`, filename, sha);
   if (!tarball) {
     this.status = 404;
-    this.body   = {error: 'no tarball found'};
+    this.body = {
+      error: 'no tarball found'
+    };
     return;
   }
   this.set('Content-Length', tarball.size);
@@ -101,55 +110,74 @@ app.use(r.get('/:scope/:name/-/:filename/:sha', function *(scope, name, filename
 }));
 
 // get package tarball without sha
-app.use(r.get('/:name/-/:filename', function *(name, filename) {
+app.use(r.get('/:name/-/:filename', function * (name, filename) {
   let ext = path.extname(filename);
   filename = path.basename(filename, ext);
   this.redirect(`/${name}/-/${filename}/a${ext}`);
 }));
 
+// get package tarball with sha 
+app.use(r.get('/:name/-/:nameversion/:sha/:filename', function * (name, nameversion, sha, filename) {
+  let ext = path.extname(filename);
+  // filename = path.basename(filename, ext);
+  this.redirect(`/${name}/-/${nameversion}${ext}/${sha}`);
+}));
+
 // login
-app.use(r.put('/-/user/:user', function *() {
+app.use(r.put('/-/user/:user', function * () {
   let auth = yield user.authenticate(yield parse(this));
   if (auth) {
     this.status = 201;
-    this.body   = {token: auth};
+    this.body = {
+      token: auth
+    };
   } else {
     this.status = 401;
-    this.body   = {error: "invalid credentials"};
+    this.body = {
+      error: "invalid credentials"
+    };
   }
 }));
 
 // authenticate
-app.use(function* (next) {
+app.use(function * (next) {
   if (!this.headers.authorization) {
     this.status = 401;
-    this.body   = {error: 'no credentials provided'};
+    this.body = {
+      error: 'no credentials provided'
+    };
     return;
   }
   let token = this.headers.authorization.split(' ')[1];
   this.username = yield user.findByToken(token);
   if (!this.username) {
     this.status = 401;
-    this.body   = {error: 'invalid credentials'};
+    this.body = {
+      error: 'invalid credentials'
+    };
     return;
   }
   yield next;
 });
 
 // whoami
-app.use(r.get('/-/whoami', function *() {
-  this.body = {username: this.username};
+app.use(r.get('/-/whoami', function * () {
+  this.body = {
+    username: this.username
+  };
 }));
 
 // npm publish
-app.use(r.put('/:name', function *() {
+app.use(r.put('/:name', function * () {
   let pkg = yield parse(this);
   try {
     yield packages(this.metric).upload(pkg);
     this.body = yield packages(this.metric).get(pkg.name);
   } catch (err) {
     if (err === packages(this.metric).errors.versionExists) {
-      this.body   = {error: err.toString()};
+      this.body = {
+        error: err.toString()
+      };
       this.status = 409;
     } else {
       throw err;
@@ -159,7 +187,7 @@ app.use(r.put('/:name', function *() {
 
 module.exports = app;
 if (!module.parent) {
-  app.listen(app.port, function () {
+  app.listen(app.port, function() {
     console.error(`${app.name} listening on port ${app.port} [${app.env}]`);
   });
 }
